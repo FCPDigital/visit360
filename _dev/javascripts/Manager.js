3d3056dc7391541cea9d7e42be50b796186e10e0
SolalDR <solal.dussout-revel@hotmail.fr>
2018-04-23 08:48:02 -0700

    Improve code quality in Manager.js

--- _dev/javascripts/Manager.js
+++ _dev/javascripts/Manager.js
@@ -1,6 +1,7 @@
-
-
-
+/**
+ * Manager of all visit
+ * @param {HTMLNode} el 
+ */
 function MapManager(el) {
 	this.el = el;
 	this.maps = [];
@@ -23,18 +24,44 @@ MapManager.prototype = {
 		navWidth: 150,
 		offset: 10,
 		width: 500,
-		height: 500
+		height: 500,
+		breakPoint: 650
 	},
 
-	createMarkerUtil: function(){
-		var el = document.createElement("div");
-		el.id = "visite-marker-transition-util"; 
-		el.className = "marker marker--overall marker--no-transition";
-		document.body.appendChild(el);
-		el.innerHTML = "<div class='marker__point'></div>"
-		this.markerUtil = new Marker(el, null);
+	/**
+	 * @return {Integer} The height of all visit 
+	 */
+	get height(){
+		var offset = 0;
+		if( this.isResponsive ){
+			offset = 100;
+		}
+		return this.maps.length * this.config.height * 0.2 + (this.maps.length - 1) * this.config.step + offset; 
 	},
 
+	/**
+	 * @return {Integer} The offset (used to calculate space between maps)
+	 */
+	get offset(){
+		return this.isResponsive ? this.config.offset + 50 : this.config.offset;
+	},
+
+	/**
+	 * Create an invisible marker used for transition
+	 */
+	createMarkerUtil: function() {
+		var element = document.createElement("div");
+		element.id = "visite-marker-transition-util"; 
+		element.className = "marker marker--overall marker--no-transition";
+		element.innerHTML = "<div class='marker__point'></div>"
+		document.body.appendChild(element);
+		this.markerUtil = new Marker(element, null);
+	},
+
+	/**
+	 * Return the map from it ID 
+	 * @param {Integer?} id
+	 */
 	getMapRankFromId: function(id){
 		for(var i=0; i<this.maps.length; i++){
 			if(this.maps[i].id == id) {
@@ -44,6 +71,10 @@ MapManager.prototype = {
 		return null;
 	},
 
+	/**
+	 * Choose regular mode (the original mode)
+	 * Minify all maps
+	 */
 	regular: function(){
 		
 		var n = this.maps.length;
@@ -58,10 +89,15 @@ MapManager.prototype = {
 		this.closeMapButton.classList.remove("map__close--display");
 
 		this.nav.unSelectAll();
+
 	},
 
+	/**
+	 * Focus on a specific map 
+	 * This mode is fired on map's hover
+	 * @param {Integer?} rank
+	 */
 	focus: function(rank){
-		
 		var n = this.maps.length;
 		var h = n - 1 * this.config.step; 
 
@@ -73,44 +109,35 @@ MapManager.prototype = {
 				decal = -20;
 			}
 
-			if( rank == i ){
-				this.maps[i].fade = false;
-			} else {
-				this.maps[i].fade = true;
-			}
+			this.maps[i].fade = rank == i ? false : true;
 			this.maps[i].setPosition(-1*i * this.config.step - h/2 + this.offset + decal);
 		}
+
 		this.mode = "focus";
 	},
 
+	/**
+	 * Select a specific map
+	 * This mode is fired when the user click on a map. 
+	 * @param {Integer} rank
+	 */
 	select: function(rank) {
 
 		for(var i=0; i<this.maps.length; i++){
 			if( i < rank ){
-				this.maps[i].toTop();
+				this.maps[i].toTop(); // If before target, hide by top slide
 			} else if( i > rank ) {
-				this.maps[i].toBottom();
+				this.maps[i].toBottom(); // If after target, hide by bottom slide
 			} else {
-				this.maps[i].select();
+				this.maps[i].select(); // If good : select, save currentMap and current mode
 				this.currentMap = this.maps[i];
 				this.mode = "map";
 			}
 		}
 
+		// Update right navigation and display close button
 		this.nav.selectFromMap(this.currentMap);
 		this.closeMapButton.classList.add("map__close--display");
-	},
-
-	get height(){
-		var offset = 0;
-		if( this.isResponsive ){
-			offset = 100;
-		}
-		return this.maps.length * this.config.height * 0.2 + (this.maps.length - 1) * this.config.step + offset; 
-	},
-
-	get offset(){
-		return this.isResponsive ? this.config.offset + 50 : this.config.offset;
 	},
 
 
@@ -130,34 +157,13 @@ MapManager.prototype = {
 		this.nav.desktop();
 	},
 
+	/**
+	 * Refresh all the markers position when a resizing is done
+	 */
 	refreshAllMarkersPosition: function() {
 		for(var i=0; i<this.maps.length; i++) {
 			this.maps[i].refreshMarkerPositions();
 		}
-	},
-
-	updateBoundaries: function(){
-		var w = this.el.offsetWidth; 
-		if( w < this.config.width + this.config.navWidth || window.innerWidth < 650 ) {
-			this.responsive();
-		} else {
-			this.desktop();
-		}
-
-		this.refreshAllMarkersPosition();
-
-		switch (this.mode){
-			case "regular": this.regular(); break;
-			case "focus": this.focus(); break;
-		}
-
-		setTimeout(this.refreshAllMarkersPosition.bind(this), 500)
-
-		for(var i=0; i<this.maps.length; i++){
-			this.maps[i].refreshBoundaries();
-		}
-
-		this.container.style.height = this.height + "px";
 	},
 
 
@@ -167,8 +173,41 @@ MapManager.prototype = {
 	//
 	////////////////////////////////
 
+	/**
+	 * Used in window.onresize & initialisation
+	 */
+	onResize: function(){
+		var w = this.el.offsetWidth; 
 
-	initEvent: function(map, rank){
+		// Manage responsive mode
+		if( w < this.config.width + this.config.navWidth || window.innerWidth < this.config.breakPoint ) {
+			this.responsive();
+		} else {
+			this.desktop();
+		}
+
+		switch (this.mode){
+			case "regular": this.regular(); break;
+			case "focus": this.focus(); break;
+		}
+
+		// refresh boundaries and markers position
+		for(var i=0; i<this.maps.length; i++){
+			this.maps[i].refreshBoundaries();
+		}
+		this.refreshAllMarkersPosition();
+		setTimeout(this.refreshAllMarkersPosition.bind(this), 500)
+
+		// Refresh height from getter
+		this.container.style.height = this.height + "px";
+	},
+
+	/**
+	 * Init single map event
+	 * @param {Map} map
+	 * @param {Integer} rank
+	 */
+	initMapEvent: function(map, rank){
 		var self = this; 
 
 		map.el.addEventListener("mouseenter", function(){
@@ -188,12 +227,18 @@ MapManager.prototype = {
 		})
 	},
 
+	/**
+	 * Init all the events
+	 */
 	initEvents: function(){
 		var self = this;
+
+		// Maps events
 		for(var i=0; i<this.maps.length; i++){
-			this.initEvent(this.maps[i], i);
+			this.initMapEvent(this.maps[i], i);
 		}
 
+		// Close button click
 		this.closeMapButton.addEventListener("click", function(){
 			self.regular();
 			for(var i=0; i<self.maps.length; i++){
@@ -201,18 +246,20 @@ MapManager.prototype = {
 			}
 		})
 
-		window.addEventListener("resize", self.updateBoundaries.bind(this));
+		// Onresize
+		window.addEventListener("resize", self.onResize.bind(this));
 	},
 
-
+	/**
+	 * Initialisation
+	 */
 	init: function(){
 		var maps = this.el.querySelectorAll(".map"); 	
 		for(var i=0; i<maps.length; i++){
 			this.maps.push(new MapItem(maps[i], this));
 		}
 
-		this.updateBoundaries();
-	
+		this.onResize();
 		this.initEvents();
 		this.regular();
 	}
